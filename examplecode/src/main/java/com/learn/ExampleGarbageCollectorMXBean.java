6ffec76fe28f716b4e7dd1ee515a29626d46d05c
John Harby <flexablesoft@gmail.com>
2021-10-08 07:21:24 -0700

    Added gc class removed test

---
+++ examplecode/src/main/java/com/learn/ExampleGarbageCollectorMXBean.java
@@ -0,0 +1,20 @@
+package com.learn;
+
+import java.lang.management.ManagementFactory;
+
+public class ExampleGarbageCollectorMXBean {
+    public static void main(String[] args) {
+        // Run 100 GCs
+        for (int i = 0; i < 100; i++) {
+            System.gc();
+        }
+
+        // Print basic information from available beans
+        for (final var bean : ManagementFactory.getGarbageCollectorMXBeans()) {
+            System.out.println(bean.getName());
+            System.out.println("   Count: " + bean.getCollectionCount());
+            System.out.println("   Total Time: " + bean.getCollectionTime() + "ms");
+            System.out.println("   Average Time: " + (bean.getCollectionTime() / (double)bean.getCollectionCount()) + "ms");
+        }
+    }
+}
